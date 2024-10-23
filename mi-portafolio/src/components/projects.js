b34148892603bde784a1e0d420cb56bee43d93de
Lorena-Rodriguez <lorenarodriguezc@gmail.com>
2024-10-23 17:25:59 +0200

    add project component

--- mi-portafolio/src/components/projects.js
+++ mi-portafolio/src/components/projects.js
@@ -1,11 +1,50 @@
 import React from 'react';
 import '../styles/projects.css';
+import galeria from '../assets/galeriadearte.svg';
+import oviva from '../assets/oviva.svg';
+import ruleta from '../assets/ruleta.svg';
+import market from '../assets/marketplace.svg'
+import githubicon from '../assets/github.svg';
 
 function Projects () {
     return (
-<div>
-    <h1>Proyectos</h1>
-</div>
+    <div className='projects-container'>
+
+        <div className='title'>
+            <p className='title-text'>Proyectos Personales</p>
+        </div>
+
+    <div className='projects-line1'>
+        <div className='galeria-arte'>
+            <p>Galería de Arte</p>
+            <img src={galeria} alt='galeria de arte'/>
+            <img src={githubicon} alt='github icono' />
+        </div>
+
+        <div className='oviva'>
+            <p>Oviva</p>
+            <img src={oviva} alt='galeria de arte'/>
+            <img src={githubicon} alt='github icono' />
+        </div>
+
+        <div className='ruleta'>
+            <p>Ruleta de la Fortuna</p>
+            <img src={ruleta} alt='galeria de arte'/>
+            <img src={githubicon} alt='github icono' />
+        </div>
+
+        <div className='marketplace'>
+            <p>Marketplace</p>
+            <img src={market} alt='galeria de arte'/>
+            <img src={githubicon} alt='github icono' />
+        </div>
+
+
+    </div>
+
+
+    </div>
+
     );
 };
 
