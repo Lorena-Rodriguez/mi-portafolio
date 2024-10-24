081f849e488e93f3f7e9c118502c8439b0d31aeb
Lorena-Rodriguez <lorenarodriguezc@gmail.com>
2024-10-24 17:46:57 +0200

    add links to Navbar buttons

--- mi-portafolio/src/components/navbar.js
+++ mi-portafolio/src/components/navbar.js
@@ -15,11 +15,16 @@ function Navbar () {
         <img src={home_icon} className='image-icon-navbar' />
     </div>
 
-    <div className='buttons-navbar-box'>
+    {/* <div className='buttons-navbar-box'>
         <button className='buttons-navbar'>LinkedIn</button>
         <button className='buttons-navbar'>Github</button>
         <button className='buttons-navbar'>CV</button>
         <button className='buttons-navbar'>Contacto</button>
+    </div> */}
+
+    <div className='buttons-navbar-box'>
+        <a href='https://www.linkedin.com/in/lorenarodriguezcortes/' className='buttons-navbar'  target="_blank" rel="noopener noreferrer">LinkedIn</a>
+        <a href='https://github.com/Lorena-Rodriguez' className='buttons-navbar'  target="_blank" rel="noopener noreferrer">Github</a>
     </div>
 
     </div>
