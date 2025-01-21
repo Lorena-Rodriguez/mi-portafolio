b62a7d1021a6dff5f0da7c22dc439960766edd0d
Lorena-Rodriguez <lorenarodriguezc@gmail.com>
2025-01-21 18:12:33 +0100

    fix experience texts

--- mi-portafolio/src/components/experience.js
+++ mi-portafolio/src/components/experience.js
@@ -16,27 +16,19 @@ function Experience () {
         </div>
         <div className='textcontainer'>
             <p className='blocktext'>
-            <span className='bold-text'>2024 | Asociación Arrabal-AID , Factoría F5 y somos F5 | Bootcamp Desarrollo web
-            FullStack</span>
-            <p>He desarrollado 9 proyectos donde se han reforzado los siguientes stacks: 
-            HMTL, CSS,
-            Javascript, React, Localstorage, Jsonserver, MySQL, Python, Flask, API, Insomnia,
-            Node, Bootstrap, Flexbox/Grid, Atomic Design, UX/UI, Git/Github, Agile, Scrum, Clean
-            Code, Pair programming. Los proyectos se han realizado en grupo, cada vez con distintas personas, pudiendo
-            desarrolllar Softskills diferentes, entre ellas: Comunicación efectiva, trabajo en equipo,
-            resolución de problemas, adaptabilidad, gestión del tiempo, empatía.</p>
+            <span className='bold-text'>2024 | Prácticas de Desarrollo Web en Sun Agile Software</span>
+            <p>Mapeo de código en lenguaje C#, preparación de documentación para el cliente. Power BI</p>
             </p>
 
             <p className='blocktext'>
             <span className='bold-text'>2014 - 2024 | Otros:</span>
-            <p>Estudio de Oposiciones, Teleoperadora, telemarketing, azafata de congresos, camarera.</p> 
+            <p>Estudio de Oposiciones, Teleoperadora, telemarketing.</p> 
             </p>
 
             <p className='blocktext'>
             <span className='bold-text'>2013 - 2014 | Grupo Vértice | Autora de Contenidos</span>
             <p>Autora de libros de texto de temática de desarrollo web (HTML, CSS y JavaScript) para
-            la obtención del certificado de profesionalidad.</p>
-            <p>Búsqueda de información, documentación.</p>
+            la obtención del certificado de profesionalidad. Búsqueda de información, documentación.</p>
             </p>
 
             <p className='blocktext'>
