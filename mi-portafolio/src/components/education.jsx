64c7bfe487a9859a9e9e4074376b49bd3b3eee22
Lorena-Rodriguez <lorenarodriguezc@gmail.com>
2025-01-21 18:53:34 +0100

    add text to education

--- mi-portafolio/src/components/education.jsx
+++ mi-portafolio/src/components/education.jsx
@@ -3,8 +3,80 @@ import '../styles/education.css'
 function Education () {
     return (
 <>
-<div>
-    
+<div className='educationContainer'>
+
+        <div>
+            <p>
+                Formación Reglada
+            </p>
+            <p>
+            Filosofía - Universidad de Málaga - octubre 2001 / junio 2005 | Estudios inacabados (4 de 5 años).
+            </p>
+            <p>
+            Inglés: B1 - Cambridge - septiembre 2020
+            </p>
+        </div>
+
+        <div>
+            <p>
+                Cursos
+            </p>
+        </div>
+
+        <div>
+            <p>
+            JavaScript Moderno | En curso.
+            </p>
+            <p>
+                Curso de Udemy de Javascript, actualizado a 2025.
+            </p>
+            <p>
+            Desarrollo Web
+            </p>
+            <p>
+            BOOTCAMP Desarrollo Web Fullstack | 2024 - 850 horas
+            </p>
+            <p>
+            Bootcamp de Desarrollo Web Full Stack Impartido por Asociación Arrabal en formato híbrido donde se desarrollaron las siguientes aptitudes: Web Services API · Capacidad de razonamiento · Scrum · Frameworks de JavaScript · Figma · Desarrollo web · Bases de datos · React.js · Metodologías ágiles · Bootstrap · Python · SQL · Diseño de la interfaz de usuario · HTML5 · Control de versiones · JavaScript · React Hooks · MySQL · Trello · Gestión de proyectos · GitHub · CSS3
+            </p>
+            <p>
+            Python | 2024 - 200 horas
+            </p>
+            <p>
+            Curso de Introducción a la Programación impartido por “Códigofacilito” en formato Bootcamp online, usando el lenguaje Python.
+            </p>
+            <p>
+            Curso Introducción a la Programación | 200 horas
+            </p>
+            <p>
+            Curso impartido por IBM, online: HTML, CSS, Python. 
+            </p>
+        </div>
+        <div>
+            <p>
+            Diseño Web
+            </p>
+            <p>
+            Diseño Web y Multimedia | 384 horas
+            </p>
+            <p>
+            Curso dedicado al conocimiento de la estructura de interfaz y elementos de diseño web y multimedia, diseño e implementación de la estructura de un sitio web, tratamiento informatizado de imágenes, programación de diseños interactivos y animaciones multimedia (HTML, CSS, Flash).
+            </p>
+            <p>
+            Diseño Gráfico
+            </p>
+            <p>
+            Técnico Auxiliar en Diseño Industrial | 650 horas
+            </p>
+            <p>
+            Curso dedicado al conocimiento de procesos de fabricación en diseño industrial, elaboración de maquetas volumétricas, Autocad, Photoshop, documentación técnica del proyecto.
+            </p>
+            <p>Técnico Auxiliar en Diseño Gráfico | 604 horas
+            </p>
+            <p>
+            Curso dedicado al conocimiento del lenguaje y diseño gráfico, procesos de producción gráfica, Photoshop, Illustrator, Indesign.
+            </p>
+        </div>
 </div>
 </>
 
