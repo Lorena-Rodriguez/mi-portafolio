182bceb4a6cd293240f633f587572c21198c1a14
Lorena-Rodriguez <lorenarodriguezc@gmail.com>
2025-06-09 17:54:18 +0200

    add contact button to navbar, delete contact button from home and fix styles

--- mi-portafolio/src/components/navbar.jsx
+++ mi-portafolio/src/components/navbar.jsx
@@ -2,29 +2,29 @@ import '../styles/navbar.css';
 //import home_icon from '../assets/home_icon.svg'
 
 function Navbar () {
-
     return (
-
-    <div className='navbar'>
-
-    <div className='description-text-box'>
-    <p className='description-text'>Web Developer</p>
-    </div>
-    
-
-    {/* <div className='icon-home'>
-        <img src={home_icon} className='image-icon-navbar' />
-    </div> */}
-
-    <div className='buttons-navbar-box'>
-        <a href='#project' className='buttons-navbar'  rel="noopener noreferrer">Proyectos</a>
-        <a href='#stacks' className='buttons-navbar'  rel="noopener noreferrer">Stacks</a>
-        <a href='#experience' className='buttons-navbar'   rel="noopener noreferrer">Experiencia</a>
-        <a href='#training' className='buttons-navbar' rel="noopener noreferrer">Formación</a>
-    </div>
-
-    </div>
-
+        <nav className='navbar' aria-label="Menú principal">
+            <div className='description-text-box'>
+                <p className='description-text'>Web Developer</p>
+            </div>
+            <ul className='buttons-navbar-box'>
+                <li>
+                    <a href='#project' className='buttons-navbar' rel="noopener noreferrer" title="Ir a Proyectos">Proyectos</a>
+                </li>
+                <li>
+                    <a href='#stacks' className='buttons-navbar' rel="noopener noreferrer" title="Ir a Stacks">Stacks</a>
+                </li>
+                <li>
+                    <a href='#experience' className='buttons-navbar' rel="noopener noreferrer" title="Ir a Experiencia">Experiencia</a>
+                </li>
+                <li>
+                    <a href='#training' className='buttons-navbar' rel="noopener noreferrer" title="Ir a Formación">Formación</a>
+                </li>
+                <li>
+                    <a href='#contactform' className='buttons-navbar' rel="noopener noreferrer" title="Ir a Contacto">Contacto</a>
+                </li>
+            </ul>
+        </nav>
     );
 };
 
